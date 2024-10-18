ba3631efddefb2f5438ee463b20b99d0c736f856
Husban-Ali <alihusban458@gmail.com>
2024-10-18 21:43:33 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-const dotenv = require("dotenv");
 const connectDB = require("./Config/db.js");
 const app = require("./app.js");
-
+const dotenv = require("dotenv");
 dotenv.config();
+
 connectDB();
 
-const PORT = process.env.PORT || 8001;
+const PORT = 8001;
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
 });
