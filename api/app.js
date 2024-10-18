090b7fb4578068dbd66280c7b0a4b0b860570098
Husban-Ali <alihusban458@gmail.com>
2024-10-18 21:02:47 +0500

    update

--- api/app.js
+++ api/app.js
@@ -4,11 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
