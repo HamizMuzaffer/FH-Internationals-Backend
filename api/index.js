b389452e54c7d32b4657acf6ddce2be19c3cfc25
Husban-Ali <alihusban458@gmail.com>
2024-10-19 16:11:21 +0500

    update

--- api/index.js
+++ api/index.js
@@ -10,3 +10,7 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
 });
+
+export default (req, res) => {
+  res.status(200).json({ message: "Hello, World!" });
+};
