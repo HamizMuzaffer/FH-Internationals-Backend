c5a82061bed61ce9a0ae1af2f81b8511c81144f7
Husban-Ali <alihusban458@gmail.com>
2024-10-20 16:23:43 +0500

    error-fixed

--- api/Routes/Routes.js
+++ api/Routes/Routes.js
@@ -2,8 +2,8 @@ import express from "express";
 import { registerUser, loginUser } from "../Controllers/userController.js";
 import {
   createOrder,
-  getOrders,
-  getOrderById,
+  // getOrders,
+  // getOrderById,
   getAllOrders,
   updateOrderStatus,
   trackOrder,
@@ -15,8 +15,8 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/orders", protect, createOrder);
-router.get("/orders", protect, getOrders);
-router.get("/orders/:id", protect, getOrderById);
+// router.get("/orders", protect, getOrders);
+// router.get("/orders/:id", protect, getOrderById);
 router.get("/admin/orders", protect, getAllOrders);
 router.put("/admin/update-status", protect, updateOrderStatus);
 router.get("/track/:trackingNumber", trackOrder);
--- api/index.js
+++ api/index.js
@@ -10,7 +10,3 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
 });
-
-export default (req, res) => {
-  res.status(200).json({ message: "Hello, World!" });
-};
