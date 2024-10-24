475aad950d250d122855e054eda3f609d9d9afcb
Husban-Ali <alihusban458@gmail.com>
2024-10-24 15:49:04 +0500

    update cors

--- api/app.js
+++ api/app.js
@@ -4,7 +4,25 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fh-internationals.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
