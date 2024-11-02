4be9797ff8b417f728730df44737a6884232ad67
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-11-02 16:19:03 +0500

    initial

--- api/app.js
+++ api/app.js
@@ -4,25 +4,25 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://fh-internationals.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://fh-internationals.vercel.app",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 app.options("*", cors());
 app.use(express.json());
