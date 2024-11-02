d2013d0675effb6f2137628c3b9136a46d85255a
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-11-02 19:15:23 +0500

    middleware added

--- api/Controllers/orderController.js
+++ api/Controllers/orderController.js
@@ -61,26 +61,26 @@ export const createOrder = async (req, res) => {
   }
 };
 
-// export const getOrders = async (req, res) => {
-//   try {
-//     const orders = await Order.find({ userId: req.user._id });
-//     res.json(orders);
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
+export const getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.user._id });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
-// export const getOrderById = async (req, res) => {
-//   try {
-//     const order = await Order.findById(req.params.id);
-//     if (!order) {
-//       return res.status(404).json({ message: "Order not found" });
-//     }
-//     res.status(200).json(order);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
+export const getOrderById = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const getAllOrders = async (req, res) => {
   if (req.user.username !== "admin") {
